cab6b334f3b4c7917b67b1ce092d50090cb64eb1
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-02-24 21:11:56 +0000

    New

--- UILibrary.lua
+++ UILibrary.lua
@@ -33,6 +33,7 @@ local globalConfFilePath = 'Aztup Hub V3/configs/globalConf.bin';
 local isGlobalConfigOn = readfile(globalConfFilePath) == 'true';
 --]]
 
+--[[
 local Signal = {}
 Signal.__index = Signal
 
@@ -80,6 +81,61 @@ function Signal:Destroy()
 
     self.BindableEvent:Destroy()
     self.BindableEvent = nil
+end
+--]]
+
+local Signal = {}
+Signal.__index = Signal
+Signal.ClassName = "Signal"
+
+function Signal.new()
+	local self = setmetatable({}, Signal)
+
+	self._bindableEvent = Instance.new("BindableEvent")
+	self._argData = nil
+	self._argCount = nil -- Prevent edge case of :Fire("A", nil) --> "A" instead of "A", nil
+
+	return self
+end
+
+function Signal.isSignal(object)
+	return typeof(object) == 'table' and getmetatable(object) == Signal;
+end;
+
+function Signal:Fire(...)
+	self._argData = {...}
+	self._argCount = select("#", ...)
+	self._bindableEvent:Fire()
+	self._argData = nil
+	self._argCount = nil
+end
+
+function Signal:Connect(handler)
+	if not self._bindableEvent then return error("Signal has been destroyed"); end --Fixes an error while respawning with the UI injected
+
+	if not (type(handler) == "function") then
+		error(("connect(%s)"):format(typeof(handler)), 2)
+	end
+
+	return self._bindableEvent.Event:Connect(function()
+		handler(unpack(self._argData, 1, self._argCount))
+	end)
+end
+
+function Signal:Wait()
+	self._bindableEvent.Event:Wait()
+	assert(self._argData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
+	return unpack(self._argData, 1, self._argCount)
+end
+
+function Signal:Destroy()
+	if self._bindableEvent then
+		self._bindableEvent:Destroy()
+		self._bindableEvent = nil
+	end
+
+	self._argData = nil
+	self._argCount = nil
 end
 
 local library = {
